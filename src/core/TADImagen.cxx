56aeef67d468eeef7b0932f623f57adef92b6ea6
Achalogy <achalogy@gmail.com>
2025-02-18 11:18:35 -0500

    Implementar TAD Imagen #28

---
+++ src/core/TADImagen.cxx
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#include "TADImagen.h"
+
+Imagen::Imagen(unsigned short r_ancho, unsigned short r_alto, unsigned short r_max_intensidad, std::vector<std::vector<unsigned short>> r_contenido)
+{
+  // Verificar valores
+  if (r_ancho < 0 || r_alto < 0)
+    throw std::out_of_range("Dimensiones invalidas");
+  if (r_max_intensidad < 0 || r_max_intensidad > 255)
+    throw std::out_of_range("Intensidad maxima invalida, debe estar entre 0 y 255");
+
+  // Verificar contenido
+
+  bool matriz_invalida = false;
+
+  for (std::vector<unsigned short> i : r_contenido)
+  {
+    if (matriz_invalida)
+      break;
+
+    if (r_contenido.size() != r_ancho)
+      matriz_invalida = true;
+  }
+
+  if (matriz_invalida)
+    throw std::invalid_argument("La dimension de la matriz no concuerda con los datos de alto y ancho");
+
+  // Datos verificados
+
+  this->ancho = r_ancho;
+  this->alto = r_alto;
+  this->contenido = r_contenido;
+}
+
+unsigned short Imagen::getAlto()
+{
+  return this->alto;
+}
+unsigned short Imagen::getAncho()
+{
+  return this->ancho;
+}
+unsigned short Imagen::getMaxIntensidad()
+{
+  return this->max_intensidad;
+}
+std::vector<std::vector<unsigned short>> Imagen::getContenido()
+{
+  return this->contenido;
+}
---
+++ src/core/TADImagen.h
@@ -0,0 +1,61 @@
+#ifndef TADIMAGEN_H
+#define TADIMAGEN_H
+
+#include <bits/stdc++.h>
+
+/**
+ * @file TADImagen.h
+ * @brief Definición de la clase Imagen
+ */
+
+/**
+ * @class Imagen
+ * @brief Clase que implementa el TAD Imagen.
+ */
+
+class Imagen
+{
+private:
+  unsigned short ancho;          ///< Ancho de la imagen
+  unsigned short alto;           ///< Alto de la imagen
+  unsigned short max_intensidad; ///< Maxima intensidad de iluminación en la imagen
+
+  // Contenido de la imagen
+  std::vector<std::vector<unsigned short>> contenido; ///< Matriz 2D que contiene la imagen, cada valor esta entre 0 y 255, representa que tan iluminado esta el pixel
+
+public:
+  /**
+   * @brief Constructor de la imagen, revisa cada dato antes de crear una imagen
+   * @param r_ancho Ancho de la imagen
+   * @param r_alto Alto de la imagen
+   * @param r_max_intensidad Maxima intesidad de iluminación en la imagen
+   * @param r_contenido Matriz 2D que contiene la imagen, cada valor esta entre 0 y 255, representa que tan iluminado esta el pixel
+   */
+  Imagen(unsigned short r_ancho, unsigned short r_alto, unsigned short r_max_intensidad, std::vector<std::vector<unsigned short>> r_contenido);
+
+  /**
+   * @brief Retorna el ancho de la imagen
+   * @return Ancho de la imagen
+   */
+  unsigned short getAncho();
+
+  /**
+   * @brief Retorna el alto de la imagen
+   * @return Alto de la imagen
+   */
+  unsigned short getAlto();
+
+  /**
+   * @brief Retorna la intensidad maxima de la imagen
+   * @return Intensidad maxima de la imagen
+   */
+  unsigned short getMaxIntensidad();
+
+  /**
+   * @brief Retorna el contenido de la imagen
+   * @return Contenido de la imagen
+   */
+  std::vector<std::vector<unsigned short>> getContenido();
+};
+
+#endif
