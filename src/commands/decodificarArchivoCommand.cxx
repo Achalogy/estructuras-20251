5f91ab269648efb277f2a40081f6dc716896d720
Achalogy <achalogy@gmail.com>
2025-04-18 11:54:35 -0500

    Documentación decodificarArchivoCommand.cxx

--- src/commands/decodificarArchivoCommand.cxx
+++ src/commands/decodificarArchivoCommand.cxx
@@ -1,21 +1,49 @@
+/**
+ * @file decodificarArchivoCommand.cxx
+ * @brief Implementación del comando encargado de decodificar un archivo que
+ * debe contener una imagen en formato PGM códificada haciendo uso del algoritmo
+ * de Huffman
+ *
+ * Este archivo contiene la implementación de la función
+ * `handlerDecodificarImagen`, que genera un nuevo archivo PGM (argv[2]) con el
+ * contenido decodificado de la imagen almacenada en el archiv argv[1].
+ */
+
 #include <bits/stdc++.h>
+
 #include <fstream>
+
 #include "../core/TADCommandManager.h"
 #include "../core/TADImagen.h"
-#include "../utils/leerImagenHuff.h"
 #include "../utils/guardarImagenPGM.h"
+#include "../utils/leerImagenHuff.h"
 
 using namespace std;
 
-int handlerDecodificarArchivo(vector<string> argv, Memoria &memoria)
-{
-  try
-  {
+/**
+ * @brief Decodifica y genera el archivo PGM de una imagen codificada con el
+ * algoritmo de Huffman
+ *
+ * @param argv Vector de strings que contiene los argumentos del usuario.
+ *        - `argv[0]`: Nombre del programa (ignorado).
+ *        - `argv[1]`: (Obligatorio) Nombre del archivo binario codificado con
+ * el algoritmo Huffman
+ *        - `argv[2]`: (Obligatorio) Nombre del archivo PGM destino de la
+ * información decodificada desde el archivo binario
+ *
+ * @param memoria Memoria del sistema
+ *
+ * @return 0 si la ejecución fue exitosa.
+ */
+
+int handlerDecodificarArchivo(vector<string> argv, Memoria &memoria) {
+  try {
     cout << "Comenzando a cargar el archivo..." << endl;
     Imagen *imagen = leerImagenHuff(argv[1]);
     memoria.setImagenEnMemoria(imagen);
 
-    cout << "Se ha finalizado la carga y dedodificacion del archivo " << argv[1] << endl;
+    cout << "Se ha finalizado la carga y dedodificacion del archivo " << argv[1]
+         << endl;
 
     guardarImagenPGM(imagen, argv[2]);
 
@@ -27,10 +55,7 @@ int handlerDecodificarArchivo(vector<string> argv, Memoria &memoria)
   }
 }
 
-Comando CommandManager::decodificarArchivoCommand = *(
-    new Comando({"decodificar_archivo",
-                 3,
-                 [](vector<string> args, Memoria &memoria)
-                 {
-                   return handlerDecodificarArchivo(args, memoria);
-                 }}));
+Comando CommandManager::decodificarArchivoCommand = *(new Comando(
+    {"decodificar_archivo", 3, [](vector<string> args, Memoria &memoria) {
+       return handlerDecodificarArchivo(args, memoria);
+     }}));
