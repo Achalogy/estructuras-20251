85c6d265a451e38974942b701d4d0504e9a45f6e
Achalogy <achalogy@gmail.com>
2025-04-18 19:14:10 -0500

    liberar memoria

--- src/commands/codificarImagenCommand.cxx
+++ src/commands/codificarImagenCommand.cxx
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
+
 #include <fstream>
+
 #include "../core/TADCommandManager.h"
 #include "../core/TADImagen.h"
 #include "../core/huffman/Huffman.h"
@@ -7,46 +9,34 @@
 
 using namespace std;
 
-int handlerCodificarImagen(vector<string> argv, Memoria &memoria)
-{
-    Imagen *img = memoria.getImagenEnMemoria();
+int handlerCodificarImagen(vector<string> argv, Memoria &memoria) {
+  Imagen *img = memoria.getImagenEnMemoria();
 
-    if (img == nullptr)
-    {
-        cout << "No hay una imagen cargada en memoria" << endl;
-        return 0;
-    }
+  if (img == nullptr) {
+    cout << "No hay una imagen cargada en memoria" << endl;
+    return 0;
+  }
 
-    Huffman h;
+  Huffman h;
 
-    cout << "Codificando imagen..." << endl;
+  cout << "Codificando imagen..." << endl;
 
-    h.genTree(img->getContenido());
+  h.genTree(img->getContenido());
 
-    // h.writeGraph();
-    cout << "Guardando archivo " << argv[1] << endl;
+  // h.writeGraph();
+  cout << "Guardando archivo " << argv[1] << endl;
 
-    try
-    {
-        guardarImagenHUFF(
-            img,
-            h.getReps(),
-            h.encode(img->getContenido()),
-            argv[1]);
-        cout << "El archivo se ha guardado con exito" << endl;
-        return 0;
-    }
-    catch (const std::exception &err)
-    {
-        cerr << "Error: " << err.what();
-        return 1;
-    }
+  try {
+    guardarImagenHUFF(img, h.getReps(), h.encode(img->getContenido()), argv[1]);
+    cout << "El archivo se ha guardado con exito" << endl;
+    return 0;
+  } catch (const std::exception &err) {
+    cerr << "Error: " << err.what();
+    return 1;
+  }
 }
 
-Comando CommandManager::codificarImagenCommand = *(
-    new Comando({"codificar_imagen",
-                 2,
-                 [](vector<string> args, Memoria &memoria)
-                 {
-                     return handlerCodificarImagen(args, memoria);
-                 }}));
+Comando CommandManager::codificarImagenCommand = *(new Comando(
+    {"codificar_imagen", 2, [](vector<string> args, Memoria &memoria) {
+       return handlerCodificarImagen(args, memoria);
+     }}));
--- src/commands/decodificarArchivoCommand.cxx
+++ src/commands/decodificarArchivoCommand.cxx
@@ -1,36 +1,39 @@
 #include <bits/stdc++.h>
+
 #include <fstream>
+
 #include "../core/TADCommandManager.h"
 #include "../core/TADImagen.h"
-#include "../utils/leerImagenHuff.h"
 #include "../utils/guardarImagenPGM.h"
+#include "../utils/leerImagenHuff.h"
 
 using namespace std;
 
-int handlerDecodificarArchivo(vector<string> argv, Memoria &memoria)
-{
-  try
-  {
+int handlerDecodificarArchivo(vector<string> argv, Memoria &memoria) {
+  Imagen *imagen = NULL;
+
+  try {
     cout << "Comenzando a cargar el archivo..." << endl;
     Imagen *imagen = leerImagenHuff(argv[1]);
     memoria.setImagenEnMemoria(imagen);
 
-    cout << "Se ha finalizado la carga y dedodificacion del archivo " << argv[1] << endl;
+    cout << "Se ha finalizado la carga y dedodificacion del archivo " << argv[1]
+         << endl;
 
     guardarImagenPGM(imagen, argv[2]);
+
+    if (imagen != NULL) delete imagen;
 
     std::cout << "Archivo " << argv[2] << " guardado con exito" << std::endl;
     return 0;
   } catch (const std::exception &err) {
+    if (imagen != NULL) delete imagen;
     std::cerr << "ERROR " << argv[1] << ": " << err.what() << endl;
     return 1;
   }
 }
 
-Comando CommandManager::decodificarArchivoCommand = *(
-    new Comando({"decodificar_archivo",
-                 3,
-                 [](vector<string> args, Memoria &memoria)
-                 {
-                   return handlerDecodificarArchivo(args, memoria);
-                 }}));
+Comando CommandManager::decodificarArchivoCommand = *(new Comando(
+    {"decodificar_archivo", 3, [](vector<string> args, Memoria &memoria) {
+       return handlerDecodificarArchivo(args, memoria);
+     }}));
